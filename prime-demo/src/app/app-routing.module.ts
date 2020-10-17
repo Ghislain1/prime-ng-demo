e4f9692f2c8ebc6df16d74f859471bbb5fdc4b32
Ghislain <Gzeleu@gmx.de>
2020-10-17 13:40:44 +0200

    Git hub pahe bae doen

--- prime-demo/src/app/app-routing.module.ts
+++ prime-demo/src/app/app-routing.module.ts
@@ -3,7 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { GetStartedComponent } from './components/get-started/get-started.component';
 
-const routes: Routes = [{ path: 'started', component: GetStartedComponent }];
+const routes: Routes = [
+  { path: 'started', component: GetStartedComponent },
+  {
+    path: 'car',
+    loadChildren: () => import('./car/car.module').then(m => m.CarModule),
+
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
